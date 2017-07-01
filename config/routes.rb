972f07dcc08305e9b5a356834a43dd4ab1ca9c89
Zach Nusbaum <zachdnusbaum@gmail.com>
2017-07-01 15:02:54 -0700

    rails_apps_composer: set up mail_view for development

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,4 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+mount MailPreview => 'mail_view' if Rails.env.development?end
